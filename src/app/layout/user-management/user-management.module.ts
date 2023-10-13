d62f148effa135a4bb8d1461ef5ff56f6bea42c4
Justine Edward Santos <hydxenonmain@gmail.com>
2023-10-13 13:29:59 +0800

    Add User Management layout

--- src/app/layout/user-management/user-management.module.ts
+++ src/app/layout/user-management/user-management.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserManagementComponent } from './user-management.component';
 import { UserManagementRoutingModule } from './user-management-routing.module';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    UserManagementComponent
+    UserManagementComponent,
+    MyAccountComponent,
+    DatePickerComponent
   ],
   imports: [
     CommonModule,
     UserManagementRoutingModule,
+    FormsModule
     
   ]
 })
