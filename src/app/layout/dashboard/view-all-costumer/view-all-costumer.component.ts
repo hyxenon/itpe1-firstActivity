f061d86c58261d0e8848f04b348838f7047fb349
Justine Edward Santos <hydxenonmain@gmail.com>
2023-10-14 12:25:19 +0800

    added view modal

---
+++ src/app/layout/dashboard/view-all-costumer/view-all-costumer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-costumer',
+  templateUrl: './view-all-costumer.component.html',
+  styleUrls: ['./view-all-costumer.component.scss']
+})
+export class ViewAllCostumerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
